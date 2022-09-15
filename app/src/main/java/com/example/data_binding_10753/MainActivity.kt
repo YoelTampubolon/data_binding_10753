5fd517a8100b8cd90395d742612a186a5eca4391
Yoel Otniel P.M.T (200710753) <yoeltampubolon457@gmail.com>
2022-09-15 14:53:43 +0700

    Data Binding

---
+++ app/src/main/java/com/example/data_binding_10753/MainActivity.kt
@@ -0,0 +1,27 @@
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.example.data_binding_10753.databinding.ActivityMainBinding
+import androidx.lifecycle.ViewModelProvider
+import com.example.data_binding_10753.MainViewModel
+import com.example.data_binding_10753.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val mainViewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        DataBindingUtil.setContentView<ActivityMainBinding>(
+            this, R.layout.activity_main
+        ).apply{
+            this.lifecycleOwner = this@MainActivity
+            this.viewmodel = mainViewModel
+        }
+
+        mainViewModel.editTextContent.observe(this){
+            Toast.makeText(this, it, Toast.LENGTH_SHORT).show()
+        }
+    }
+}
